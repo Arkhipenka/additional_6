916c1e6319920c9f74f62fe1b9d86fd60a3e573d
Arkhipenka <europeoplebelarus@gmail.com>
2017-09-19 11:33:19 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,39 +1,71 @@
 module.exports = function zeros(expression) {
-   var arr = expression.split('*');
- var arrOfFactorials = [];
- var finalResult = 1;
- for(var i = 0;i < arr.length;i++){
-	var temp = null;
-	var subArr = arr[i].split('');
-	for(var j = 0; j < subArr.length; j++){
-		if(subArr[j] === '!'){
-			temp++;
-		}
-	}
-	var numberToFact = parseInt(arr[i]);
-	switch(temp){
-		case 1: arrOfFactorials.push(factorial(numberToFact));break;
-		case 2: arrOfFactorials.push(factorialEven(numberToFact));break;
-	}
-	
- }
-		for(var k = 0;k < arrOfFactorials.length;k++){
-			finalResult*=arrOfFactorials[k];
-		}
-		return finalResult;
- 
- function factorial(n) {
-  return n ? n * factorial(n - 1) : 1;
-}
-function factorialEven(n) {
-	if(n === 1){
-		return 1;
-	}
-	var result = 1;
-	while(n > 0){
-		result*=n;
-		n-=2;
-	}
-	return result;
-}
+    this.j=0;
+    this.zeroes=0;
+    this.numbers;
+    this.countFive=0;
+    this.evenNumbers=0;
+        for(let i=0; i<expression.length; i++)
+        {
+           if( expression[i]==="!")
+           {
+               if(expression[i+1]==="!")                            
+               {
+
+                   numbers = Number(expression.slice(j, i));
+                   if(numbers%2===0){                               
+                       for(let x=2; x<=numbers; x=x+2){
+                           if(x%10===0){
+                               zeroes=zeroes+1;
+                           }
+                           if(x%50===0){
+                               zeroes=zeroes+1;                    
+                           }
+                           evenNumbers=evenNumbers+1;
+                       }
+                   }
+                   else{                                            
+                       for(let z=1; z<=numbers; z=z+2){
+                           if(z%5===0){
+                               countFive=countFive+1;
+                           }
+                           if(z%25===0){
+                               countFive=countFive+1;
+                           }
+                       }
+                   }
+                   i = i + 3;
+                   j = i;
+               }
+               else {
+                   numbers = Number(expression.slice(j, i));        
+                   for(let k=1; k<=numbers; k++)
+                   {    if(k%5===0)
+                        {
+                            zeroes=zeroes+1;
+                        }
+                       if(k%25===0)
+                       {
+                           zeroes=zeroes+1;
+                       }
+                       if(k%2===0)
+                       {
+                           evenNumbers=evenNumbers+1;
+                       }
+                   }
+                   i = i + 2;
+                   j = i;
+               }
+           }
+
+        }
+        if(evenNumbers<countFive)
+        {
+            zeroes=zeroes+evenNumbers;
+        }
+        else
+        {
+            zeroes=zeroes+countFive;
+        }
+
+    return zeroes;
 }
