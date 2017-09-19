da0b8cd7753de2d725d53eba21248a7b377e025b
Arkhipenka <europeoplebelarus@gmail.com>
2017-09-19 12:14:03 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -10,34 +10,33 @@ module.exports = function zeros(expression) {
            {
                if(expression[i+1]==="!")                            
                {
-
-                   numbers = Number(expression.slice(j, i));
-                   if(numbers%2===0){                               
-                       for(let x=2; x<=numbers; x=x+2){
-                           if(x%10===0){
-                               this.zeroes=zeroes+1;
-                           }
-                           if(x%50===0){
-                               this.zeroes=zeroes+1;                    
-                           }
-                           this.evenNumbers=evenNumbers+1;
-                       }
-                   }
-                   else{                                            
-                       for(let z=1; z<=numbers; z=z+2){
-                           if(z%5===0){
-                               this.countFive=countFive+1;
-                           }
-                           if(z%25===0){
-                               this.countFive=countFive+1;
-                           }
-                       }
-                   }
-                   i = i + 3;
-                   j = i;
+                  this.numbers = Number(expression.slice(j, i));
+                  if(numbers%2===0){                               
+                    for(let x=2; x<=numbers; x=x+2){
+                      if(x%10===0){
+                        this.zeroes=zeroes+1;
+                        }
+                          if(x%50===0){
+                            this.zeroes=zeroes+1;                    
+                          }
+                          this.evenNumbers=evenNumbers+1;
+                    }
+                  }
+                  else{                                            
+                    for(let z=1; z<=numbers; z=z+2){
+                      if(z%5===0){
+                        this.countFive=countFive+1;
+                      }
+                        if(z%25===0){
+                          this.countFive=countFive+1;
+                        }
+                    } 
+                  }                             
+                  i = i + 3;
+                  j = i;
                }
                else {
-                   numbers = Number(expression.slice(j, i));        
+                   this.numbers = Number(expression.slice(j, i));        
                    for(let k=1; k<=numbers; k++)
                    {    if(k%5===0)
                         {
@@ -56,7 +55,6 @@ module.exports = function zeros(expression) {
                    j = i;
                }
            }
-
         }
         if(evenNumbers<countFive)
         {
@@ -66,7 +64,6 @@ module.exports = function zeros(expression) {
         {
             this.zeroes=zeroes+countFive;
         }
-
     return zeroes;
 }
 
